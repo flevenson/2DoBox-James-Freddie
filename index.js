b92f844869f32db00237e527b46f32116bcfe0ef
James Burbage <jeburbage@gmail.com>
2018-07-23 19:32:15 -0600

    Save to local storage fix

--- index.js
+++ index.js
@@ -3,49 +3,48 @@ var body = $('.body-input').val();
 var numCards = 0;
 var quality = "swill";
 
+$('.save-btn').on('click', saveCard);
 
-var newCard = function(id , title , body , quality) {
-    $(".bottom-box").prepend('<div id="' + id + '"class="card-container"><h2 class="title-of-card">'  
-            + title +  '</h2>'
-            + '<button class="delete-button"></button>'
-            +'<p class="body-of-card">'
-            + body + '</p>'
-            + '<button class="upvote"></button>' 
-            + '<button class="downvote"></button>' 
-            + '<p class="quality">' + 'quality:' + '<span class="quality">' + quality + '</span>' + '</p>'
-            + '<hr>' 
-            + '</div>');
+function saveCard(event) {
+  event.preventDefault();
+  if ($('.title-input').val() === "" || $('.body-input').val() === "") {
+    return;
+  };  
+  var card = new Card($('.title-input').val(), $('.body-input').val());
+  // numCards++;
+  addToPage(card);
+  localStoreCard(card);
+  $('form')[0].reset();
 };
 
-function cardObject(id, title, body, quality) {
+function addToPage(card) {
+    $(".bottom-box").prepend(`<div id="${card.id}"class="card-container">
+                                <h2 class="title-of-card">${card.title}</h2>
+                                <button class="delete-button"></button>
+                                <p class="body-of-card">${card.body}</p>
+                                <button class="upvote"></button>
+                                <button class="downvote"></button>
+                                <p class="quality">quality: <span class="quality">${card.quality}</span></p>
+                              </div>`);
+};
+
+function Card(title, body) {
         this.id = $.now();
-        this.title = $('.title-input').val();
-        this.body = $('.body-input').val();
+        this.title = title;
+        this.body = body;
         this.quality = quality || 'swill';
     };
 
-$.each(localStorage, function(key) {
-    var cardData = JSON.parse(this);
-    numCards++;
-    $( ".bottom-box" ).prepend(newCard(key, cardData.title, cardData.body, cardData.quality));
-});
+// $.each(localStorage, function(key) {
+//     var cardData = JSON.parse(this);
+//     numCards++;
+//     $( ".bottom-box" ).prepend(newCard(key, cardData.title, cardData.body, cardData.quality));
+// });
 
-var localStoreCard = function() {
-    var cardString = JSON.stringify(cardObject());
-    localStorage.setItem('card' + numCards  , cardString);
+function localStoreCard(card) {
+    var cardString = JSON.stringify(card);
+    localStorage.setItem(card.id, cardString);
 }
-
-$('.save-btn').on('click', function(event) {
-    event.preventDefault();
-    if ($('.title-input').val() === "" || $('.body-input').val() === "") {
-       $('save-btn').disabled = true;
-    };  
-
-    numCards++;
-    $( ".bottom-box" ).prepend(newCard('card' + numCards, $('.title-input').val(), $('.body-input').val(), quality)); 
-    localStoreCard();
-    $('form')[0].reset();
-});
 
 $(".bottom-box").on('click', function(event){
     var currentQuality = $($(event.target).siblings('p.quality').children()[0]).text().trim();
@@ -90,6 +89,7 @@ $(".bottom-box").on('click', function(event){
     else if (event.target.className === "delete-button") {
         var cardHTML = $(event.target).closest('.card-container').remove();
         var cardHTMLId = cardHTML[0].id;
+  localStoreCard(card);
         localStorage.removeItem(cardHTMLId);
     }
 });
