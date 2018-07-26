196f845d0cc59f311e8d8282a9ff80eb9abe08e1
“Freddie <freddielev@gmail.com>
2018-07-25 22:02:31 -0600

    Add aria labels

--- index.js
+++ index.js
@@ -1,47 +1,43 @@
-// var title = $(".title-input").val();
-// var task = $(".task-input").val();
-// var importance = "Normal";
-
 $(".save-btn").on("click", saveCard);
 $('.bottom-box').on('click', '.delete-button', deleteTask);
 $(window).on("load", retrieveLocalStorage);
-$('.bottom-box').on('keyup', '.title-of-card', changeContent)
-$('.bottom-box').on('keyup', '.task-of-card', changeContent)
+$('.bottom-box').on('keyup', '.title-of-card', changeContent);
+$('.bottom-box').on('keyup', '.task-of-card', changeContent);
 
 function saveCard(event) {
   event.preventDefault();
   if ($(".title-input").val() === "" || $(".task-input").val() === "") {
     return;
-  }
+  };
   var card = new Card($(".title-input").val(), $(".task-input").val());
   addToPage(card);
   localStoreCard(card);
   $("form")[0].reset();
-}
+};
 
 function addToPage(card) {
   $(".bottom-box").prepend(`<div id="${card.id}"class="card-container">
-                                <h2 class="title-of-card" contenteditable="true">${card.title}</h2>
-                                <button class="delete-button"></button>
-                                <p class="task-of-card" contenteditable="true">${card.task}</p>
-                                <button class="upvote"></button>
-                                <button class="downvote"></button>
-                                <p class="importance">Importance: <span class="importance">${card.importance}</span>
+                                <h2 class="title-of-card" contenteditable="true" aria-label="your editable task title">${card.title}</h2>
+                                <button class="delete-button" aria-label="click here to delete task"></button>
+                                <p class="task-of-card" contenteditable="true" aria-label="your editable task description">${card.task}</p>
+                                <button class="upvote" aria-label="click here to increase the importance of your task"></button>
+                                <button class="downvote" aria-label="click here to lower the importance of your task"></button>
+                                <p class="importance">Importance: <span class="importance" aria-label="your task importance level">${card.importance}</span>
                                 </p>
                               </div>`);
-}
+};
 
 function Card(title, task, importance) {
   this.id = $.now();
   this.title = $(".title-input").val();
   this.task = $(".task-input").val();
   this.importance = importance || "Normal";
-}
+};
 
 function localStoreCard(card) {
   var cardString = JSON.stringify(card);
   localStorage.setItem(card.id, cardString);
-}
+};
 
 function changeContent(e){
   var cardID = $(e.target).parent().attr("id");
@@ -60,7 +56,7 @@ function retrieveLocalStorage() {
     var storedCard = JSON.parse(retrievedKey);
     addToPage(storedCard);
   });
-}
+};
 
 $(".bottom-box").on("click", function(event) {
   var currentImportance = $($(event.target).siblings("p.importance").children()[0]).text().trim();
@@ -71,7 +67,7 @@ $(".bottom-box").on("click", function(event) {
   } else if (event.target.className === "downvote") {
     downvoteFunctionality(currentImportance);
     localStoreCard(card);
-  }
+  };
 });
 
 function downvoteFunctionality(currentImportance) {
@@ -83,8 +79,8 @@ function downvoteFunctionality(currentImportance) {
     $($(event.target).siblings("p.importance").children()[0]).text(importance);
   } else if (currentImportance === "Normal") {
     importance = "Normal";$($(event.target).siblings("p.importance").children()[0]).text(importance);
-  }
-}
+  };
+};
 
 function upvoteFunctionality(currentImportance) {
   if (currentImportance === "High") {
@@ -94,14 +90,14 @@ function upvoteFunctionality(currentImportance) {
     $($(event.target).siblings("p.importance").children()[0]).text(importance);
   } else if (currentImportance === "Critical") {
     importance = "Critical";$($(event.target).siblings("p.importance").children()[0]).text(importance);
-  }
-}
+  };
+};
 
 function deleteTask(e) {
   $(e.target).parent().remove();
   var currentTaskId = $(e.target).parent().attr('id');
   localStorage.removeItem(currentTaskId);
-}
+};
 
 // var cardHTML = $(event.target).closest('.card-container');
 // var cardHTMLId = cardHTML[0].id;
